eec8cd260e50cec64fc4b1a888abd1cca5b5d79f
Kacper Skrzypek <skrzyp33k@protonmail.com>
2022-12-10 17:51:23 +0100

    implemented side drawer

---
+++ lib/gbslHomePage.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class GBSLHomePage extends StatelessWidget {
+  const GBSLHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GB Shopping List'),
+        foregroundColor: Theme.of(context).colorScheme.tertiary,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.tertiary,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
+      ),
+      body: Text('siema'),
+    );
+  }
+}
